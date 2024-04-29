9735889a5589cd01cf6e28e43a9a6a8357e548f7
martindevas <martindevas45@gmail.com>
2024-04-29 20:41:27 -0300

    Agrego Sccafold

--- app/src/main/java/com/example/notepad/Pages/MainPage.kt
+++ app/src/main/java/com/example/notepad/Pages/MainPage.kt
@@ -1,5 +1,12 @@
 package com.example.notepad.Pages
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -8,9 +15,29 @@ import com.example.notepad.ui.theme.NotepadTheme
 
 @Composable
 fun Mainpage(modifier: Modifier = Modifier) {
-    Text(
-        text = "MainPage",
-        modifier = modifier
+    Scaffold(
+        modifier = modifier,
+        topBar = {MainTopAppBar()}
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            /*TODO: Agregar el contenido*/
+        }
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopAppBar(){
+    TopAppBar(
+        title = {
+            Text(text = "Notepad")
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        )
     )
 }
 
