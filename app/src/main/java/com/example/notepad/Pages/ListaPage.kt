3f3455d82fcaedeb2a6bcc265262a5413f974edd
martindevas <martindevas45@gmail.com>
2024-05-02 15:32:50 -0300

    Agrego cart clickeable para los items de la lista

--- app/src/main/java/com/example/notepad/Pages/ListaPage.kt
+++ app/src/main/java/com/example/notepad/Pages/ListaPage.kt
@@ -1,12 +1,17 @@
 package com.example.notepad.Pages
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.notepad.ui.theme.NotepadTheme
 
 
@@ -18,7 +23,16 @@ fun ListaPage(
 ) {
   LazyColumn(modifier = modifier) {
         items(items = notas){
-            Text(text = it)
+            Card(
+                modifier = modifier
+                    .padding(10.dp)
+                    .clickable { onNotaSelected() }
+            ) {
+                Text(
+                    modifier = modifier.padding(10.dp),
+                    text = it
+                )
+            }
         }
   }
 }
@@ -26,7 +40,7 @@ fun ListaPage(
 @Preview(showBackground = true)
 @Composable
 fun ListaPagePreview() {
-    val notas = remember { mutableStateListOf<String>() }
+    val notas = remember { mutableStateListOf<String>("jojo", "lala", "hola") }
     NotepadTheme {
         ListaPage(
             onNotaSelected = {},
